611565a46ebccc8b643a4a70964de01b17f562d0
neha45556 <neha45556@gmail.com>
2019-10-22 12:08:16 -0700

    Update main.c

--- lab6syncsms/source/main.c
+++ lab6syncsms/source/main.c
@@ -8,16 +8,63 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+#include <avr/interrupt.h>
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+volatile unsigned char TimerFlag = 0;
 
-    /* Insert your solution below */
-    while (1) {
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
 
-    }
-    return 1;
+void TimerOn(){
+    TCCR1B = 0x0B;
+    
+    OCR1A = 125;
+    
+    TIMSK1 = 0x02;
+    
+    TCNT1 = 0;
+    
+    _avr_timer_cntcurr = _avr_timer_M;
+    
+    SREG |- 0x80;
+    
 }
+
+void TimerOff() {
+    TCCR1B = 0x00;
+    
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if (_avr_timer_cntcurr == 0){
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsinged long M){
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+void main() {
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    TimerSet(1000);
+    TimerOn();
+    unsigned char tmpB = 0x00;
+    while(1){
+        tmpB = ~tmpB;
+        PORTB = tmpB;
+        while(!TimerFlag);
+            TimerFlag = 0;
+     }
+}
+    
+    
