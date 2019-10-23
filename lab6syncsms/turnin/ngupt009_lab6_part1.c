491cc52bf4cbf0c2e651bb1d127bebe203e82957
neha45556 <neha45556@gmail.com>
2019-10-23 09:43:53 -0700

    Update ngupt009_lab6_part1.c

--- lab6syncsms/turnin/ngupt009_lab6_part1.c
+++ lab6syncsms/turnin/ngupt009_lab6_part1.c
@@ -27,321 +27,95 @@
 
 */
 
-
 #include <avr/io.h>
-
-
-#include <avr/interrupt.h>
-
-
-
-
-
-
+#include <arv/interrupt.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 
 volatile unsigned char TimerFlag = 0;
-
-
-
-
-
-
-
+enum states{INIT, LEDONE, LEDTWO, LEDTHREE} state;
+#define A0 (~PINA & 0x01)
 unsigned long _avr_timer_M = 1;
-
-
 unsigned long _avr_timer_cntcurr = 0;
 
-
-
-
-
-
-
 void TimerOn(){
-
-
-   TCCR1B = 0x0B;
-
-
-  
-
-
-   OCR1A = 125;
-
-
-  
-
-
-   TIMSK1 = 0x02;
-
-
-  
-
-
-   TCNT1 = 0;
-
-
-  
-
-
-   _avr_timer_cntcurr = _avr_timer_M;
-
-
-  
-
-
-   SREG |- 0x80;
-
-
-  
-
-
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;  
+}
+    
+void TimerOff(){
+    TCCR1B = 0x00;
 }
 
-
-
-
-
-
-
-void TimerOff() {
-
-
-   TCCR1B = 0x00;
-
-
-  
-
-
+void TimerISR(){
+    TimerFlag = 1;
 }
 
-
-
-
-
-
-
-void TimerISR() {
-
-
-   TimerFlag = 1;
-
-
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
 }
 
-
-
-
-
-
-
-ISR(TIMER1_COMPA_vect) {
-
-
-   _avr_timer_cntcurr--;
-
-
-   if (_avr_timer_cntcurr == 0){
-
-
-       TimerISR();
-
-
-       _avr_timer_cntcurr = _avr_timer_M;
-
-
-   }
-
-
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-
-
-
-
-
-
-void TimerSet(unsinged long M){
-
-
-   _avr_timer_M = M;
-
-
-   _avr_timer_cntcurr = _avr_timer_M;
-
-
+void runThing(unsigned char tmpB){
+	switch(state){
+		case INIT:
+		state = LEDONE;
+		break;
+		case LEDONE:
+		state = LEDTWO;
+		break;
+		case LEDTWO:
+		state = LEDTHREE;
+		break;
+		case LEDTHREE:
+		state = LEDONE;
+		break;
+	}
+	switch(state){
+		case INIT:
+		tmpB = 0x00;
+		break;
+		case LEDONE:
+		tmpB = 0x01;
+		break;
+		case LEDTWO:
+		tmpB = 0x02;
+		break;
+		case LEDTHREE:
+		tmpB = 0x04;
+		break;
+	}
+	PORTB = tmpB;
+	
 }
 
-
-
-
-
-
-
-enum States { Start, LedOn1, LedOn2, LedOn3} state;
-
-
-
-
-
-
-
-void Tick_Blink(){
-
-
-   unsigned char B0 = 0x00;
-
-
-   switch(state){
-
-
-       case Start:
-
-
-           state = LedOn1;
-
-
-           break;
-
-
-       case LedOn1:
-
-
-           state = LedOn2;
-
-
-           break;
-
-
-       case LedOn2:
-
-
-           state = LedOn3;
-
-
-           break;
-
-
-       case LedOn3:
-
-
-           state = LedOn1;
-
-
-           break;
-
-
-   }
-
-
-   switch(state){
-
-
-       case Start:
-
-
-           B0 = 0;
-
-
-           break;
-
-
-       case LedOn1:
-
-
-           B0 = 0x01;
-
-
-           break;
-
-
-       case LedOn2:
-
-
-           B0 = 0x02;
-
-
-           break;
-
-
-       case LedOn3:
-
-
-           B0 = 0x04;
-
-
-           break;
-
-
-   }
-
-
-   PORTB = B0;
-
-
-}
-
-
-           
-
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-void main() {
-
-
- 
-
-
-   DDRB = 0xFF;
-
-
-   PORTB = 0x00;
-
-
-   TimerSet(1000);
-
-
-   TimerOn();
-
-
-   unsigned char tmpB = 0x00;
-
-
-   while(1){
-
-
-       tmpB = ~tmpB;
-
-
-       PORTB = tmpB;
-
-
-       Tick_Blink();
-
-
-       while(!TimerFlag);
-
-
-           TimerFlag = 0;
-
-
+int main(void) {
+    
+    DDRB = 0xFF; DDRA = 0x00;
+    PORTB = 0x00; PORTA = 0xFF;
+    TimerSet(300);
+    TimerOn();
+    unsigned char tmpB = 0x00;
+
+    while (1) {
+        runThing(tmpB);
+        while(!TimerFlag){};
+        TimerFlag = 0;
     }
-
-
+    return 1;
 }
-
 
   
